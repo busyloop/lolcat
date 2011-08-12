9568318e50f098cfcb8e7fe3ac446657841c1481
Yu-Jie Lin <livibetter@gmail.com>
2011-08-13 00:48:08 +0800

    Catcorp is here.
    FIKS busyloop/#13

--- lib/lolcat/cat.rb
+++ lib/lolcat/cat.rb
@@ -67,7 +67,8 @@ HEADER
       opt :speed, "Animation speed", :short => 's', :default => 20.0
       opt :force, "Force color even when stdout is not a tty", :short => 'f', :default => false
       opt :version,  "Print version and exit", :short => 'v'
-      opt :help,  "Show this message", :short => 'h'
+      opt :halp,  "Show this message", :short => 'h'
+      opt :help,  "Catcorp overlords' exclusive option", :short => 'c'
       banner <<FOOTER
 
 Examples:
@@ -85,6 +86,7 @@ FOOTER
     opts = Trollop::with_standard_exception_handling p do
       begin
         o = p.parse ARGV
+        raise Trollop::HelpNeeded if o[:halp]
       rescue Trollop::HelpNeeded
         buf = StringIO.new
         p.educate buf
