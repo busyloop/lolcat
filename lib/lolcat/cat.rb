7bb8055cd46992529d1e654b4d6588314172da4e
Nick Chambers <dtscode@gmail.com>
2015-10-12 02:00:16 -0700

    added -n support

--- lib/lolcat/cat.rb
+++ lib/lolcat/cat.rb
@@ -31,7 +31,7 @@ module Lol
       :speed => 20,
       :spread => 8.0,
       :freq => 0.3,
-      :newline => true
+      :newline => false
     }.merge opts
 
     begin
@@ -67,7 +67,7 @@ HEADER
       opt :duration, "Animation duration", :short => 'd', :default => 12
       opt :speed, "Animation speed", :short => 's', :default => 20.0
       opt :force, "Force color even when stdout is not a tty", :short => 'f', :default => false
-      opt :newline, "Don't print a trailing newline with this option", :short => 'n', :default => true
+      opt :newline, "Don't print a trailing newline with this option", :short => 'n', :default => false
       opt :version,  "Print version and exit", :short => 'v'
       opt :help,  "Show this message", :short => 'h'
 
@@ -92,7 +92,7 @@ FOOTER
         buf = StringIO.new
         p.educate buf
         buf.rewind
-        halp! buf.read, {:newline => true}
+        halp! buf.read, {:newline => false}
         buf.close
       end
       o
